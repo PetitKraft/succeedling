b83c4eb272ae435ed9758daf7003dc56fb5835f0
Petit Kraft <sympacifica@gmail.com>
2022-03-12 10:14:47 +0900

    Update temps.py

--- temps.py
+++ temps.py
@@ -1,9 +1,10 @@
 #coding: utf-8
 
 import mysql.connector
-#from w1thermsensor import W1ThermSensor
-#import RPi.GPIO as GPIO
-#import time
+from w1thermsensor import W1ThermSensor
+import RPi.GPIO as GPIO
+import time
+
 
 
 # DB設定
@@ -19,10 +20,12 @@ cnx = mysql.connector.connect(**config)
 cursor = cnx.cursor(dictionary=True)
 
 
+
 # SQLのフォーマット
 register_temps = ("INSERT INTO temps "
                     "(inside_temp, outside_temp) "
                     "VALUES (%(inside_temp)s, %(outside_temp)s)")
+
 
 
 # センサーの設定 (もう一個のセンサーのID:3c01e0761dd6)
@@ -30,19 +33,42 @@ inside_sensor = W1ThermSensor(sensor_id="3c01e0761e16")
 outside_sensor = W1ThermSensor(sensor_id="3c01e0767e5b")
 
 
+
+# モータードライバー用の設定
+#lwin_lpwm = 24
+#lwin_rpwm = 23
+#rwin_lpwm = 22
+#rwin_rpwm = 27
+#GPIO.setmode(GPIO.BOARD)
+#GPIO.setup(lwin_lpwm,GPIO.OUT)
+#GPIO.setup(lwin_rpwm,GPIO.OUT)
+#GPIO.setup(rwin_lpwm,GPIO.OUT)
+#GPIO.setup(rwin_rpwm,GPIO.OUT)
+
+
+
 # 気温の取得
 inside_temp = inside_sensor.get_temperature()
 outside_temp = outside_sensor.get_temperature()
 
 
+
+# 気温に基づいてサイドビニールの開け閉め
+#if inside_temperature =    
+#    GPIO.output(lwin_lpwm, isUp)
+#    GPIO.output(lwin_rpwm, not(isUp))
+#    GPIO.output(rwin_lpwm, not(isUp))
+#    GPIO.output(rwin_rpwm, isUp)
+
+
+
+# DBへの挿入
 data = {
     "inside_temp": inside_temp,
     "outside_temp": outside_temp,
 }
-
-
-# DBへの挿入
 cursor.execute(register_temps, data)
+
 
 
 cursor.close()
