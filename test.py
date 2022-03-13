eb2de598962d6a2754bbd17e6dc6d53f21d6f8f4
Petit Kraft <sympacifica@gmail.com>
2022-03-13 10:39:41 +0900

    Change greatly

--- test.py
+++ test.py
@@ -1,23 +1,15 @@
-#coding: utf-8
-
 from __future__ import print_function
 
 from google.oauth2.credentials import Credentials
 from google.oauth2 import service_account
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
-#from w1thermsensor import W1ThermSensor
-#import RPi.GPIO as GPIO
 import datetime
-import time
 
 
-
-# SpreadSheet用設定
 SPREADSHEET_ID = '1WV3AqBbdljhG5gHZrR5qXJZwm_o5UhImrV27Vmyec9M'
 RANGE_NAME = 'Data!A2:C'
 VALUE_INPUT_OPTION = "USER_ENTERED"
-
 
 
 def main():
@@ -25,47 +17,13 @@ def main():
     Prints values from a sample spreadsheet.
     """
     creds = service_account.Credentials.from_service_account_file("service_account_credentials.json")
-        
-
-
-
-    # センサーの設定 (もう一個のセンサーのID:3c01e0761dd6)
-    #inside_sensor = W1ThermSensor(sensor_id="3c01e0761e16")
-    #outside_sensor = W1ThermSensor(sensor_id="3c01e0767e5b")
-
-
-
-    # モータードライバー用の設定
-    #lwin_lpwm = 24
-    #lwin_rpwm = 23
-    #rwin_lpwm = 22
-    #rwin_rpwm = 27
-    #GPIO.setmode(GPIO.BOARD)
-    #GPIO.setup(lwin_lpwm,GPIO.OUT)
-    #GPIO.setup(lwin_rpwm,GPIO.OUT)
-    #GPIO.setup(rwin_lpwm,GPIO.OUT)
-    #GPIO.setup(rwin_rpwm,GPIO.OUT)
-
-
-
-    # 気温の取得
-    measured_at = datetime.datetime.now().isoformat()
-    inside_temp = 12.555#inside_sensor.get_temperature()
-    outside_temp = 19.588#outside_sensor.get_temperature()
-
-
-
-    # 気温に基づいてサイドビニールの開け閉め
-    #if inside_temperature =    
-    #    GPIO.output(lwin_lpwm, isUp)
-    #    GPIO.output(lwin_rpwm, not(isUp))
-    #    GPIO.output(rwin_lpwm, not(isUp))
-    #    GPIO.output(rwin_rpwm, isUp)
-
-
     
     try:
         service = build('sheets', 'v4', credentials=creds)
+
+        measured_at = datetime.datetime.now().isoformat()
+        inside_temp = 12.555
+        outside_temp = 19.588
 
         values = [
             [
@@ -84,6 +42,10 @@ def main():
         print('{0} cells appended.'.format(result
                                            .get('updates')
                                            .get('updatedCells')))
+
+        if not values:
+            print('No data found.')
+            return
 
         print('Datetime, Inside, Outside:')
         for row in values:
